2ece9ee45acb2989db0e2965c35f89466eb7b457
believedshelf2 <32203663+believedshelf2@users.noreply.github.com>
2018-10-11 13:04:09 -0400

    Update ex5.py

--- Variables_and_printing/ex5.py
+++ Variables_and_printing/ex5.py
@@ -5,7 +5,7 @@ my_weight = 180 # lbs
 my_eyes = 'brown'
 my_teeth = 'white'
 my_hair = 'black'
-
+ //yeet
 print(f"Let's talk about {my_name}.")
 print(f"He's {my_height} inches tall.")
 print(f"He's {my_weight} pounds heavy.")
